dd15306a0b77c1972f467aab6cc20f9268ff2b1c
Ibragimov33 <79836936+Ibragimov33@users.noreply.github.com>
2022-01-17 21:49:03 +0300

    ЛР3

---
+++ php-Ibragimov/s3-4.php
@@ -0,0 +1,30 @@
+﻿<HTML>
+<HEAD> <TITLE> Вход </TITLE> </HEAD>
+<BODY>
+<FORM method="post" action="<?php print $PHP_SELF ?>">
+ Логин:
+ <br>
+ <INPUT type="text" name="a" size="5">
+ <P> <INPUT type="submit" name="obr" value="Авторизоваться">
+</FORM>
+<?php
+$s1="Ibragimov";
+$s2="Ivanov";
+$s3="Ayupova";
+$s4="Starcev";
+if($_POST["a"]==$s1){
+echo("Здравствуйте, Ибрагимов");
+} 
+elseif($_POST["a"]==$s2){
+echo("Здравствуйте, Иванов");
+}
+elseif($_POST["a"]==$s3){
+echo("Здравствуйте, Аюпова");
+}
+elseif($_POST["a"]==$s4){
+echo("Здравствуйте, Старцев");
+}
+else{
+print("вы не зарегестрированный пользователь!");}
+?>
+</BODY> </HTML>
