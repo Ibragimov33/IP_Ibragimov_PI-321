d632524c5422ab354a318ed0e78c3fe6d66e189f
Ibragimov33 <79836936+Ibragimov33@users.noreply.github.com>
2022-01-18 23:59:09 +0300

    лр№4 1 часть

--- php-Ibragimov/index.php
+++ php-Ibragimov/index.php
@@ -1,104 +1,43 @@
-﻿<!DOCTYPE html>
+<?
+require_once 'connect.php'; ?>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Ибрагимов Айдар</title>
-<link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="design/design1.css">
-
-
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сведения о прользователях сайта</title>
 </head>
-
+<h3>Ибрагимов Айдар</h3>
 <body>
-<div class = "styles">
-<p> Дата и время:
-<p> 
-<?php
- $d=date("d.m.Y H:i");
- echo($d);
-?>
-<p> Работу выполнил студент группы ПИ-321<p> 
-<p>Ибрагимов Айдар <p>
-<ul>
-<li>
-<p> Лабораторная работа№1 <p>
-<ul>
-<li>
-<a href='/lab-1-1.php'>lab-1-1</a>
-</li>
-<li>
-<a href='/lab-1-2.php'>lab-1-2</a>
-</li>
-<li>
-<a href='/lab-1-3.php'>lab-1-3</a>
-<li> 
-<a href='/lab1-5.php'>lab1-5</a>
-<li>
-<a href='/lab-2-1.php'>lab-2-1</a>
-<li>
-<a href='/lab-2-2.php'>lab-2-2</a>
-<li>
-<a href='/lab-2-3.php'>lab-2-3</a>
-<li>
-<a href='/lab-2-4.php'>lab-2-4</a>
-<li>
-<a href='/lab-2-5.php'>lab-2-5</a>
-<li>
-<a href='/lab-2-6.php'>lab-2-6</a>
-<li>
-<a href='/lab-2-7.php'>lab-2-7</a>
-<li>
-<a href='/lab-2-8.php'>lab-2-8</a>
-<li>
-<a href='/lab-2-9.php'>lab-2-9</a>
-<li>
-<a href='/lab-2-10.php'>lab-2-10</a>
-<li>
-<a href='/lab-2-11.php'>lab-2-11</a>
-<p> Лабораторная работа№2 <p>
-<ul>
-<li>
-<a href='/lab-21.php'>lab-21</a>
-<li>
-<a href='/lab-22.php'>lab-22</a>
-<li>
-<a href='/lab-23.php'>lab-23</a>
-<li>
-<a href='/lab-24.php'>lab-24</a>
-<li>
-<a href='/lab-25.php'>lab-25</a>
-<li>
-<a href='/lab-26.php'>lab-26</a>
-<p> Лабораторная работа№3 <p>
-<ul>
-<li>
-<a href='f1.html'>Упражнение 3-1</a>
-<li>
-<a href='f2.html'>Упражнение 3-2</a>
-<li>
-<a href='f3.html'>Упражнение 3-3</a>
-<li>
-<a href='f4.html'>Упражнение 3-4</a>
-<li>
-<a href='f5.html'>Упражнение 3-5</a>
-<li>
-<a href='f6.html'>Упражнение 3-6</a>
-<li>
-<a href='f7.php'>Упражнение 3-7</a>
-<li>
-<a href='s3-1.html'>Задача 3.1</a>
-<li>
-<a href='s3-2.php'>Задача 3.2</a>
-<li>
-<a href='s3-3.php'>Задача 3.3</a>
-<li>
-<a href='s3-4.php'>Задача 3.4</a>
-<li>
-<a href='s3-5.php'>Задача 3.5</a>
-<li>
-<a href='s3-6.html'>Задача 3.6</a>
-</div>
+<table border="1">
 
-<body>
+    <h2>Зарегистрированные пользователи:</h2>
+    <table border="1">
+        <tr>
+            <th> Имя </th> <th> E-mail </th>
+            <th> Редактировать </th> <th> Уничтожить </th> </tr>
+
+        <?php
+        $link = mysqli_connect($host, $user, $password, $database)
+        or die("Ошибка " . mysqli_error($link));
+
+        $result = mysqli_query($link, "SELECT id_user, user_name, user_e_mail FROM user");// запрос на выборку сведений о пользователях
+        mysqli_select_db($link, "users");
+
+        while ($row= mysqli_fetch_array($result)){// для каждой строки из запроса
+            echo "<tr>";
+            echo "<td>" . $row['user_name'] . "</td>";
+            echo "<td>" . $row['user_e_mail'] . "</td>";
+            echo "<td><a href='edit.php?id=" . $row['id_user'] . "'> Редактировать </a> </td>"; // запуск скрипта для редактирования
+            echo "<td><a href='delete.php?id=" . $row['id_user'] . "'> Удалить </a></td>"; // запуск скрипта для удаления записи
+            echo "</tr>";
+        }
+        print "</table>";
+        $num_rows = mysqli_num_rows($result); // число записей в таблице БД
+        print("<P> Всего пользователей: $num_rows </p>");
+        ?>
+        <p> <a href="new.php"> Добавить пользователя </a>
+        <li><a href="IbragimovBDstudent/index.php">Лабораторная работа № 4</a></li>
+     
+        <li><a href="IbragimovBDLab5/index.php">Лабораторная работа № 5</a></li>
+</body>
 </html>
-
